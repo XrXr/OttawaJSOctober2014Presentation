d352e0260312ee51638807f533279e6858b67f5b
XrXr <xrsquared@gmail.com>
2014-10-08 23:16:24 -0400

    Add capability to take user supplied string for text replacement

--- demoAddon/data/replace.js
+++ demoAddon/data/replace.js
@@ -1,4 +1,4 @@
-function replaceText () {
+function replaceText (groups) {
     let walker = document.createTreeWalker(
         document.body,
         NodeFilter.SHOW_TEXT,
@@ -8,13 +8,16 @@ function replaceText () {
 
     let node;
 
+    groups = groups.map(group => [new RegExp("\\b"+group[0]), group[1]]);
+
     while((node = walker.nextNode())) {
         if (node.parentElement.nodeName == "SCRIPT") {
             continue;
         }
-        node.textContent = node.textContent.replace(/\bForce/g, "Horse");
-        node.textContent = node.textContent.replace(/\bforce/g, "horse");
+        groups.map(group => node.textContent = node.textContent.replace(group[0], group[1]));
+        // node.textContent = node.textContent.replace(/\bForce/g, "Horse");
+        // node.textContent = node.textContent.replace(/\bforce/g, "horse");
     }
 }
 
-replaceText();
+self.port.on("doReplace", replaceText);
--- demoAddon/lib/main.js
+++ demoAddon/lib/main.js
@@ -1,7 +1,17 @@
 const PageMod = require("sdk/page-mod").PageMod;
 const data = require("sdk/self").data;
+const sPrefs = require("sdk/simple-prefs");
 
 PageMod({
     include: "*",
-    contentScriptFile: data.url("replace.js")
+    contentScriptFile: data.url("replace.js"),
+    onAttach: worker => worker.port.emit("doReplace", getReplaceGroup())
 });
+
+function getReplaceGroup () {
+    let pattern = sPrefs.prefs.pattern.split("|");
+    let result = sPrefs.prefs.result.split("|");
+    return pattern.map((elem, index) => {
+        return [elem, result[index]];
+    });
+}
